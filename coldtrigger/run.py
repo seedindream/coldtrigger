cdb0d64d6b7ff75b9ca813b55aef60829def8865
seedindream <li.7533@buckeyemail.osu.edu>
2017-12-12 19:34:38 -0500

    4

--- coldtrigger/run.py
+++ coldtrigger/run.py
@@ -8,13 +8,14 @@ postreqdata = json.loads(open(os.environ['req']).read())
 url = postreqdata['url']
 time_rq = time.time()
 content=urllib.urlopen(url).read()  
+time_feed = time.time()
 print(content)
 print("request time "+str(time_rq))
 cmdCommand = 'powershell D:\\home\\site\\wwwroot\\coldtrigger\\record.exe' #specify your cmd command
 process = subprocess.Popen(cmdCommand.split(), shell=True,stdout=subprocess.PIPE)
 output5, error = process.communicate()
 
-time_feed = time.time()
+
 
 response = open(os.environ['res'], 'w')
 response.write("request time "+str(time_rq)+"feedback time "+str(time_feed)+"@@"+content+"%%"+output5)
